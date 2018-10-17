d4cbc9099d91c71d4dd7aee78a87bdf5f983991f
FinalOne <lakhanrathi65@gmail.com>
2018-10-18 00:27:50 +0530

    Initial

--- app/src/main/java/com/finalone/lakhan/finalone/MainActivity.java
+++ app/src/main/java/com/finalone/lakhan/finalone/MainActivity.java
@@ -1,13 +1,25 @@
 package com.finalone.lakhan.finalone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.finalone.lakhan.finalone.location.GPS;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GPS gps;
+    private Location location;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gps = new GPS(MainActivity.this);
+        location = gps.getLocation();
+        //TODO: Add Firebase account
+        //TODO: Add icon
+        //
     }
 }
